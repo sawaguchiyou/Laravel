efbb2c1f95f4e65fe9e62526944e0f8030ed90bd
okisada <okisada09@gmail.com>
2019-04-23 15:18:22 +0900

    csv出力

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -8,14 +8,48 @@ use Illuminate\Support\Facades\DB;
 use app\Models;
 use Carbon\Carbon;
 use Validator;
+use Response;
+use App\User;
 
 class ExportController extends Controller{
+
+  // データ抽出
   public function get (Request $request){
-    echo 'get!!';
+    $list = DB::connection('mysql_product')->table('M_Product')
+      ->get();
+
+    return view('export',compact('list'));
   }
 
+  // データ出力
   public function output (Request $request){
-    echo 'output!!';
+    //
+    $list = DB::connection('mysql_product')->table('M_Product')
+      ->get();
+
+    $stream = fopen('php://output', 'w');
+    fputcsv($stream, ['商品ID', '商品名', '単価', '最終更新日時']);
+
+    foreach($list as $lists){
+      fputcsv(
+        $stream,
+        [$lists->Product_ID,
+        $lists->Product_Name,
+        $lists->Product_Val,
+        $lists->insert_date]
+      );
+    }
+
+    $headers = array(
+      'Content-Type' => 'text/csv',
+      'Content-Disposition' => 'attachment; filename="users.csv"'
+    );
+
+    return Response::make(
+      stream_get_contents($stream),
+      200,
+      $headers
+    );
   }
 }
 
--- resources/views/export.blade.php
+++ resources/views/export.blade.php
@@ -4,12 +4,12 @@
 <meta charset="UTF-8">
 <title>csv出力</title>
 <style type="text/css">
-body{background-color:#f4f4f4; text-align:center; font-size:14px; letter-spacing:1px;}
+body{background-color:#f4f4f4; text-align:center; color:#666; font-size:14px; letter-spacing:1px;}
 p{margin:0; padding:0;}
 
 .export-wrap{
-  display:block;
-  max-width:300px;
+  display:inline-block;
+  min-width:300px;
   margin:50px auto;
   padding:30px;
   background-color:#fff;
@@ -38,7 +38,7 @@ p{margin:0; padding:0;}
 }
 
 .export-btn{
-  width:31%;
+  width:100px;
   margin:0;
   padding:5px 10px;
   font-weight:bold;
@@ -51,13 +51,35 @@ p{margin:0; padding:0;}
 }
 
 .export-btn:hover{background-color:#0984e3;}
+
+.export_table{margin:20px 0; text-align:left;}
+.export_table td{padding:5px 10px; border-bottom:1px dashed #ccc;}
 </style>
 </head>
 <body>
 <form class="export-wrap" action="export" method="POST">
   {{ csrf_field() }}
-  <input class="export-btn" type="submit" value="一覧取得" formaction = "/export/get"><br><br>
+  <input class="export-btn" type="submit" value="一覧取得" formaction = "/export/get" name="export_get">
+  @isset($list)
+  <table class="export_table" width="100%">
+    <tr>
+      <td>商品ID</td>
+      <td>商品名</td>
+      <td>単価</td>
+      <td>前回更新日時</td>
+    </tr>
+    @foreach($list as $lists)
+    <tr>
+      <td>{{ $lists->Product_ID }}</td>
+      <td>{{ $lists->Product_Name }}</td>
+      <td>{{ $lists->Product_Val }}</td>
+      <td>{{ $lists->insert_date }}</td>
+    </tr>
+    @endforeach
+  </table>
   <input class="export-btn" type="submit" value="csv出力" formaction = "/export/output">
+  <input class="export-btn" type="submit" value="戻る" name="back-btn" formaction = "/back">
+  @endisset
 </form>
 </body>
 </html>
