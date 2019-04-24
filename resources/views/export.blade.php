808bd67dc7bba131c2b9dac40d8dc72e94f6bb31
okisada <okisada09@gmail.com>
2019-04-24 16:28:08 +0900

    cssシート分け：起定

--- resources/views/export.blade.php
+++ resources/views/export.blade.php
@@ -3,58 +3,8 @@
 <head>
 <meta charset="UTF-8">
 <title>csv出力</title>
-<style type="text/css">
-body{background-color:#f4f4f4; text-align:center; color:#666; font-size:14px; letter-spacing:1px;}
-p{margin:0; padding:0;}
-
-.export-wrap{
-  display:inline-block;
-  min-width:300px;
-  margin:50px auto;
-  padding:30px;
-  background-color:#fff;
-  border-radius:5px;
-  box-shadow:3px 3px 8px rgba(0, 0, 0, 0.2);
-}
-
-.kensaku-contain{
-  display:flex;
-  flex-wrap:wrap;
-  align-items: center;
-  justify-content:space-between;
-}
-
-.kensaku-text{
-  width:40%;
-  color:#666;
-  box-sizing:border-box;
-}
-
-.kensaku-textbox{
-  width:60%;
-  margin:5px 0;
-  padding:5px;
-  box-sizing:border-box;
-}
-
-.export-btn{
-  width:100px;
-  margin:0;
-  padding:5px 10px;
-  font-weight:bold;
-  border:none;
-  border-radius:3px;
-  color:#fff;
-  background-color:#74b9ff;
-  box-sizing:border-box;
-  transition:ease-in-out .2s;
-}
-
-.export-btn:hover{background-color:#0984e3;}
-
-.export_table{margin:20px 0; text-align:left;}
-.export_table td{padding:5px 10px; border-bottom:1px dashed #ccc;}
-</style>
+<link rel="stylesheet" href="{{ asset('css/default.css') }}">
+<link rel="stylesheet" href="{{ asset('css/export.css') }}">
 </head>
 <body>
 <form class="export-wrap" action="export" method="POST">
--- resources/views/mainmenu.blade.php
+++ resources/views/mainmenu.blade.php
@@ -3,58 +3,8 @@
 <head>
 <meta charset="UTF-8">
 <title>商品検索マスタ</title>
-<style type="text/css">
-body{background-color:#f4f4f4; font-size:14px; letter-spacing:1px;}
-
-.syouhin-wrap{
-    max-width:300px;
-    margin:50px auto;
-    padding:30px;
-    border-radius:5px;
-    background-color:#fff;
-    box-shadow:3px 3px 8px rgba(0,0,0,0.3);
-}
-
-.syouhin-btn{
-    width:100%;
-    margin-bottom:10px;
-    padding:8px 10px;
-    background-color:#f4f4f4;
-}
-
-.syouhin-btn,.syuuryou-btn{
-    display:block;
-    color:#666;
-    text-align:center;
-    text-decoration:none;
-    box-sizing:border-box;
-    border:1px solid #ccc;
-    transition:ease-in-out .2s;
-}
-
-.syouhin-btn:hover{
-    color:#fff;
-    background-color:#ccc;
-}
-
-.syuuryou-btn{
-    width:30%;
-    margin:0 0 0 auto;
-    padding:5px 10px;
-    color:#fff;
-    font-size:14px;
-    font-weight:bold;
-    border:none;
-    border-radius:3px;
-    background-color:#ff6b6b;
-}
-
-.syuuryou-btn:hover{
-    color:#fff;
-    background-color:#ee5253;
-}
-
-</style>
+<link rel="stylesheet" href="{{ asset('css/default.css') }}">
+<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
 <form action = "/main" method = "POST">
